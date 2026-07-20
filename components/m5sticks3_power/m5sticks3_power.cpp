918d06d6b11a5384803c7f0db569dcfe6183a506
Oluf Lorenzen <fink.github.com@mafia-server.net>
2026-07-20 08:08:32 +0200

    fix: increase power-up delay to 1000ms per SCD40 datasheet
    SCD40 datasheet specifies 1000ms power-up time before the sensor
    enters idle state and is ready for I2C communication. Previous 200ms
    was insufficient - bus scan found no devices.

--- components/m5sticks3_power/m5sticks3_power.cpp
+++ components/m5sticks3_power/m5sticks3_power.cpp
@@ -43,7 +43,7 @@ bool M5StickS3Power::init_pmic_() {
   delay(20);
   this->pm1_.setBoostEnable(true);
   this->boost_enabled_ = true;
-  delay(200);  // Allow Grove port 5V to stabilize and SCD40 to power up
+  delay(1000);  // SCD40 datasheet: 1000ms power-up time to enter idle state
 
   // PYG2_L3B_EN controls the StickS3 LCD/audio rail. Low output enables it.
   this->pm1_.gpioSetFunc(M5PM1_GPIO_NUM_2, M5PM1_GPIO_FUNC_GPIO);
